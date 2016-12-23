304ca36af4ae8140f08c56c8e24e90b1ea55db23
UnlimitedGalaxy <1464345037@qq.com>
2016-12-23 12:07:37 +0800

    test
    tses

---
+++ meadowlark.js
@@ -0,0 +1,36 @@
+/**
+ * Created by 星辰galaxy on 2016/12/23.
+ */
+
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+	res.type('text/plain');
+	res.send('Meadowlark Travel');
+})
+
+app.get('/about', function (req, res) {
+	res.type('text/plain');
+	res.send('About Meadowlark Travel');
+})
+//custom 404 page
+app.use(function (req, res) {
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+//custom 500 page
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Sever Error');
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Express started on http://localhost:' + app.get('port') + ': press Ctrl+C to terminate.');
+})
